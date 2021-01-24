4e8c33b88f7fe5b8b4ad405e4fd5d9da4ea833fa
vrenegfl <florian.vrenegor@pentasys.de>
2021-01-24 14:58:01 +0100

    add listView with weights

--- app/src/main/java/com/example/tracker/MainActivity.java
+++ app/src/main/java/com/example/tracker/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.tracker;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -19,12 +24,19 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String FIRESTORE_LOG_TAG = "Firestore";
+
+    private WeightAdapter adapter;
 
     private FirebaseFirestore db;
 
@@ -36,8 +48,11 @@ public class MainActivity extends AppCompatActivity {
         final EditText weightEditText = findViewById(R.id.weight_edit_text);
         Button saveWeightButton = findViewById(R.id.save_weight_button);
         Button loadWeightButton = findViewById(R.id.load_weight_button);
+        ListView weightListView = findViewById(R.id.weight_list_view);
 
         db = FirebaseFirestore.getInstance();
+        adapter = new WeightAdapter();
+        weightListView.setAdapter(adapter);
 
         saveWeightButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,6 +100,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void readWeights() {
+        adapter.weights.clear();
         db.collection("weights")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -92,12 +108,74 @@ public class MainActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
+                                long timeInMillis = (long) document.getData().get("time in milliseconds");
+                                double weightInKgs = (double) document.getData().get("weight in kilograms");
+                                adapter.weights.add(new WeightDto(timeInMillis, weightInKgs));
                                 Log.d(FIRESTORE_LOG_TAG, document.getId() + " => " + document.getData());
                             }
+                            adapter.notifyDataSetChanged();
                         } else {
                             Log.w(FIRESTORE_LOG_TAG, "Error getting documents.", task.getException());
                         }
                     }
                 });
+    }
+
+    class WeightDto {
+        private final static String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+        private final long timeInMillis;
+        private final double weightInKgs;
+
+        public WeightDto(long timeInMillis, double weightInKgs) {
+            this.timeInMillis = timeInMillis;
+            this.weightInKgs = weightInKgs;
+        }
+
+        public String getDateString() {
+            SimpleDateFormat formatter = new SimpleDateFormat(DATE_FORMAT, Locale.GERMANY);
+            return formatter.format(new Date(timeInMillis));
+        }
+
+        public String getWeightString() {
+            return weightInKgs + " kg";
+        }
+    }
+
+    class WeightAdapter extends BaseAdapter {
+
+        public List<WeightDto> weights = new ArrayList<>();
+
+        @Override
+        public int getCount() {
+            return weights.size();
+        }
+
+        @Override
+        public WeightDto getItem(int position) {
+            return weights.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            final View result;
+            if (convertView == null) {
+                result = LayoutInflater.from(parent.getContext()).inflate(R.layout.weight, parent, false);
+            } else {
+                result = convertView;
+            }
+
+            WeightDto item = getItem(position);
+
+            ((TextView) result.findViewById(R.id.date_text_view)).setText(item.getDateString());
+            ((TextView) result.findViewById(R.id.weight_text_view)).setText(item.getWeightString());
+
+            return result;
+        }
     }
 }
