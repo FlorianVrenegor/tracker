44f5a29b93d7f53d1506378368165564b0cc4db7
vrenegfl <florian.vrenegor@pentasys.de>
2021-02-04 17:33:03 +0100

    add second fragment, add viewpager

--- app/src/main/java/com/example/tracker/MainActivity.java
+++ app/src/main/java/com/example/tracker/MainActivity.java
@@ -3,13 +3,54 @@ package com.example.tracker;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int NUM_PAGES = 2;
+
+    private ViewPager2 viewPager;
+    private FragmentStateAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.view_pager);
+        pagerAdapter = new ScreenSlidePagerAdapter(this);
+        viewPager.setAdapter(pagerAdapter);
     }
 
+    @Override
+    public void onBackPressed() {
+        if (viewPager.getCurrentItem() == 0) {
+            super.onBackPressed();
+        } else {
+            viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
+        }
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStateAdapter {
+        public ScreenSlidePagerAdapter(FragmentActivity fa) {
+            super(fa);
+        }
+
+        @Override
+        public Fragment createFragment(int position) {
+            if (position == 0) {
+                return new WeightFragment();
+            } else {
+                return new TimeFragment();
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return NUM_PAGES;
+        }
+    }
 }
---
+++ app/src/main/java/com/example/tracker/TimeFragment.java
@@ -0,0 +1,10 @@
+package com.example.tracker;
+
+import androidx.fragment.app.Fragment;
+
+public class TimeFragment extends Fragment {
+
+    public TimeFragment() {
+        super(R.layout.fragment_time);
+    }
+}
