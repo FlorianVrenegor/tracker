beafc78267a99fb39607704fe6c713d06068f941
vrenegfl <florian.vrenegor@pentasys.de>
2021-02-09 16:27:04 +0100

    add WeightViewModel

---
+++ app/src/main/java/com/example/tracker/weight/WeightViewModel.java
@@ -0,0 +1,13 @@
+package com.example.tracker.weight;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class WeightViewModel extends AndroidViewModel {
+
+    public WeightViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
