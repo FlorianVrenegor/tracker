3a453fa2ab03cfb531de2ee864669e2d88b4bf3b
Florian Vrenegor <florian.vrenegor@gmail.com>
2024-10-06 14:31:29 +0200

    convert main activity to kotlin

--- app/src/main/java/com/example/tracker/MainActivity.kt
+++ app/src/main/java/com/example/tracker/MainActivity.kt
@@ -1,41 +1,36 @@
-package com.example.tracker;
+package com.example.tracker
 
-import android.os.Bundle;
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
-import androidx.viewpager2.widget.ViewPager2;
+class MainActivity : AppCompatActivity() {
+    private var viewPager: ViewPager2? = null
+    private var pagerAdapter: FragmentStateAdapter? = null
 
-import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.tabs.TabLayoutMediator;
+    private val tabNames = arrayOf("Weight", "Time", "Todo")
 
-public class MainActivity extends AppCompatActivity {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-    private ViewPager2 viewPager;
-    private FragmentStateAdapter pagerAdapter;
+        viewPager = findViewById(R.id.view_pager)
+        viewPager!!.setUserInputEnabled(false)
+        pagerAdapter = ScreenSlidePagerAdapter(this)
+        viewPager!!.setAdapter(pagerAdapter)
 
-    private String[] tabNames = new String[]{"Weight", "Time", "Todo"};
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        viewPager = findViewById(R.id.view_pager);
-        viewPager.setUserInputEnabled(false);
-        pagerAdapter = new ScreenSlidePagerAdapter(this);
-        viewPager.setAdapter(pagerAdapter);
-
-        TabLayout tabLayout = findViewById(R.id.tab_layout);
-        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> tab.setText(tabNames[position])).attach();
+        val tabLayout = findViewById<TabLayout>(R.id.tab_layout)
+        TabLayoutMediator(tabLayout, viewPager!!) { tab: TabLayout.Tab, position: Int -> tab.setText(tabNames[position]) }.attach()
     }
 
-    @Override
-    public void onBackPressed() {
-        if (viewPager.getCurrentItem() == 0) {
-            super.onBackPressed();
+    override fun onBackPressed() {
+        if (viewPager!!.currentItem == 0) {
+            super.onBackPressed()
         } else {
-            viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
+            viewPager!!.currentItem -= 1
         }
     }
 }
